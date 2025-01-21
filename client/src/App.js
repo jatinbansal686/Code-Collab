9dec0453d9433be9886b165d20322b6620df0ebe
jatinbansal686 <jatinbansal686@gmail.com>
2025-01-21 18:55:48 +0530

    React version changes and editor page started

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/editor/:roomId " element={<EditorPage />} />
+      <Route path="/editor/:roomId" element={<EditorPage />} />
     </Routes>
   );
 }
--- client/src/component/EditorPage.js
+++ client/src/component/EditorPage.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "./Client";
 
 function EditorPage() {
-  return <div>EditorPage</div>;
+  const [clients, setClient] = useState([
+    { socketId: 1, username: "jatin" },
+    { socketId: 2, username: "gourvi" },
+  ]);
+
+  return (
+    <div className="container-fluid vh-100">
+      <div className="row h-100">
+        <div
+          className="col-md-2 bg-dark text-light d-flex flex-column h-100"
+          style={{ boxShadow: "2px 0px 4px rgba(0,0,0,0.1)" }}
+        >
+          <img
+            src="/images/codecast.png"
+            alt="codecast logo"
+            className="img-fluid mx-auto"
+            style={{ maxWidth: "150px", marginTop: "-43px" }}
+          />
+          <hr style={{ marginTop: "-3rem" }} />
+          <div className="d-flex flex-column overflow-auto">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+          <div className="mt-auto">
+            <hr />
+            <button className="btn btn-success">Copy Room Id</button>
+            <button className="btn btn-danger mt-2 mb-2 px-3 btn-block">
+              Leave Room
+            </button>
+          </div>
+        </div>
+        <div className="col-md-10 text-light d-flex flex-column h-100">
+          editor
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EditorPage;
