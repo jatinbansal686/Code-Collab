78ac9f0c74dc48f650a2e804fb79989a72d14c5f
jatinbansal686 <jatinbansal686@gmail.com>
2025-01-23 12:49:55 +0530

    Room id created and toasts created

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Home from "./component/Home";
 import EditorPage from "./component/EditorPage";
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/editor/:roomId" element={<EditorPage />} />
-    </Routes>
+    <>
+      <Toaster position="top-center " />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/editor/:roomId" element={<EditorPage />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { v7 as uuid } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUserName] = useState("");
+  const navigate = useNavigate();
+
+  const generateRoomId = (e) => {
+    e.preventDefault();
+    try {
+      const id = uuid();
+      setRoomId(id);
+      toast.success("Room Id is generated");
+    } catch (error) {
+      console.log("error");
+      toast.error("Failed to generate id");
+    }
+  };
+
+  const joinRoom = (e) => {
+    if (!roomId || !username) {
+      toast.error("Both fields are required");
+      return;
+    }
+    // navigate
+    navigate(`/editor/${roomId}`, {
+      state: { username },
+    });
+    toast.success("Room is joined");
+  };
+
   return (
     <div>
       <div className="container-fluid">
@@ -17,6 +48,10 @@ function Home() {
                 <h4 className="text-light">Enter the Room Id</h4>
                 <div className="form-group">
                   <input
+                    value={roomId}
+                    onChange={(e) => {
+                      setRoomId(e.target.value);
+                    }}
                     type="text"
                     className="form-control mb-2"
                     placeholder="ROOM ID"
@@ -24,12 +59,19 @@ function Home() {
                 </div>
                 <div className="form-group">
                   <input
+                    value={username}
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
                     type="text"
                     className="form-control mb-2"
                     placeholder="Username"
                   />
                 </div>
-                <button className="btn btn-success btn-lg btn-block">
+                <button
+                  onClick={joinRoom}
+                  className="btn btn-success btn-lg btn-block"
+                >
                   JOIN
                 </button>
                 <p className="mt-3 text-light">
@@ -37,6 +79,7 @@ function Home() {
                   <span
                     className="text-success p-2"
                     style={{ cursor: "pointer" }}
+                    onClick={generateRoomId}
                   >
                     New Room
                   </span>
